5146aa281892ae8f7332117a896eb0078e9933fc
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-15 18:43:11 -0600

    Create func for screen scale, position & rotation for 3d island

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,22 @@ import Island from "../models/Island";
 }
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    const screenPosition = [0, -6.5, -43];
+    const rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -22,7 +38,11 @@ const Home = () => {
         <pointLight />
         <spotLight />
         <hemisphereLight />
-        <Island />
+        <Island
+          position={islandPosition}
+          scale={islandScale}
+          rotation={islandRotation}
+        />
       </Canvas>
     </section>
   );
