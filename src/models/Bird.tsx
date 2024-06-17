8b1b6021ab48eb7b1d5fa9c3a882c17ca151eb66
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-17 16:13:20 -0600

    Import 3d model to plane & bird components

--- src/models/Bird.tsx
+++ src/models/Bird.tsx
@@ -1,5 +1,16 @@
+import { useGLTF } from "@react-three/drei";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import birdScene from "../assets/3d/bird.glb";
+
 const Bird = () => {
-  return <mesh></mesh>;
+  const { scene } = useGLTF(birdScene);
+
+  return (
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+      <primitive object={scene} />
+    </mesh>
+  );
 };
 
 export default Bird;
--- src/models/Plane.tsx
+++ src/models/Plane.tsx
@@ -1,5 +1,16 @@
+import { useGLTF } from "@react-three/drei";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import planeScene from "../assets/3d/plane.glb";
+
 const Plane = () => {
-  return <mesh></mesh>;
+  const { scene } = useGLTF(planeScene);
+
+  return (
+    <mesh>
+      <primitive object={scene} />
+    </mesh>
+  );
 };
 
 export default Plane;
