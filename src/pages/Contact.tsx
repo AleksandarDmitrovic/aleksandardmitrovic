db08d608e532aa62d7eb890441037c52e6462458
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-18 17:51:31 -0600

    Initialize contact form page with name and form state

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+
+  const handleChange = () => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1>Get In Touch</h1>
+        <form className="w-full flex flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="input"
+            placeholder="Odin"
+            required
+            value={form.name}
+            onChange={handleChange}
+          ></input>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
-  const [currentStage, setCurrentStage] = useState(0);
+  const [currentStage, setCurrentStage] = useState();
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
