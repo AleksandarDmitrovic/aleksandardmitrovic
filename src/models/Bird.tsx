c4708c79a70cb32dc4ef2da21a2169b52affda65
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-17 20:23:52 -0600

    Add animations to Bird comp

--- src/models/Bird.tsx
+++ src/models/Bird.tsx
@@ -1,13 +1,20 @@
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import birdScene from "../assets/3d/bird.glb";
 
 const Bird = () => {
-  const { scene } = useGLTF(birdScene);
+  const birdRef = useRef();
+  const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef);
+
+  useEffect(() => {
+    actions["Take 001"]?.play();
+  }, []);
 
   return (
-    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+    <mesh ref={birdRef} position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
       <primitive object={scene} />
     </mesh>
   );
