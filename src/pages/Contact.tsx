aa9c2d6c5827ae7410a13a3d01e7e23ab8ee091a
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-18 17:56:55 -0600

    Add email and message inputs + onFocus & onBlue functions

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,22 +4,56 @@ const Contact = () => {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
 
   const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1>Get In Touch</h1>
         <form className="w-full flex flex-col gap-7 mt-14">
-          <label className="text-black-500 font-semibold">Name</label>
-          <input
-            type="text"
-            name="name"
-            className="input"
-            placeholder="Odin"
-            required
-            value={form.name}
-            onChange={handleChange}
-          ></input>
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Odin"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            ></input>
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="odin@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            ></input>
+          </label>
+          <label className="text-black-500 font-semibold">
+            Message
+            <textarea
+              name="message"
+              className="textarea"
+              rows={4}
+              placeholder="Let me know how I can help you"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            ></textarea>
+          </label>
         </form>
       </div>
     </section>
