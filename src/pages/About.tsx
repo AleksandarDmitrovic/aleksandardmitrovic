3421029eb46a6171074ea6a527de98e6fa28822b
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-19 21:52:18 -0600

    Add header and headliner to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,21 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Aleksandar
+        </span>
+      </h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Web Developer based in Calgary, Canada with industry domain experience
+          in agriculture, transport, and travel.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
